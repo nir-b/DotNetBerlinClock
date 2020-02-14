17efd81d980839b8b3abe0a6e1e3adb73ad65ef1
Nirmalya Bhattacharyya <hungryandrestless@gmail.com>
2020-02-14 10:21:30 +0100

    Adding the files

--- Classes/TimeConverter.cs
+++ Classes/TimeConverter.cs
@@ -9,7 +9,13 @@ namespace BerlinClock
     {
         public string convertTime(string aTime)
         {
-            throw new NotImplementedException();
+            //Here is where we could have made this code a lot more robust, by converting the string object
+            //to DateTime using DateTime.TryParse() and using the DateTime object as an argument to Clock
+            //We could have used the power of the FCL DateTime class and filtered out any string formatting errors
+            //However we cannot really do that since 24:00:00 is an invalid time string as per the DateTime class
+            //Also it is very odd and very non-standard to have two representations that is 00:00:00 and 24:00:00 of the 
+            //exact same instant of time.
+            return new Clock().SetTime(aTime).ToString();
         }
     }
 }
